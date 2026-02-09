56bdf87b4e4ec2bf39bb9a0acf4ff6cc8c6d3ce4
JDegruson <jdegrusondev@gmail.com>
2026-02-09 13:24:39 +0100

    add building in questiondto

--- src/main/java/goodshi/ageofquizz/dto/QuestionDTO.java
+++ src/main/java/goodshi/ageofquizz/dto/QuestionDTO.java
@@ -6,10 +6,10 @@ import goodshi.ageofquizz.entity.Question;
 
 public record QuestionDTO(Integer id, String libelle, Question.QuestionTheme theme, Question.QuestionType type,
 		Question.QuestionStatus status, Question.QuestionCivilisation civilisation, String fileUrl,
-		String authorUsername, List<AnswerDTO> answers) {
+		String authorUsername, List<AnswerDTO> answers, Question.QuestionBuilding building) {
 	public static QuestionDTO fromEntity(Question q) {
 		return new QuestionDTO(q.getId(), q.getLibelle(), q.getTheme(), q.getType(), q.getStatus(), q.getCivilisation(),
 				q.getFileUrl(), q.getAuthor().getUsername(),
-				q.getAnswers().stream().map(AnswerDTO::fromEntity).toList());
+				q.getAnswers().stream().map(AnswerDTO::fromEntity).toList(), q.getBuilding());
 	}
 }
